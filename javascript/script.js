d2178e2ccc3bd907b1bc42b48fe7f6e531393442
Elliott Chong <elliottchong16@gmail.com>
2022-07-21 07:39:52 +0800

    k

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,17 @@
-let progress_bar = document.getElementById("progress-bar");
+const progress_bar = document.getElementById("progress-bar");
 
+// progress scroll bar logic
 document.addEventListener("scroll", (e) => {
   let percentage =
-    (window.scrollY / document.documentElement.scrollHeight) * 100;
+    (window.scrollY /
+      (document.documentElement.scrollHeight - window.innerHeight)) *
+    100;
   progress_bar.style.width = percentage + "%";
 });
+
+setTimeout(() => {
+  document.querySelectorAll(".anim").forEach((elt) => {
+    elt.style.transform = "translate(0)";
+    elt.style.opacity = "1";
+  });
+}, 200);
