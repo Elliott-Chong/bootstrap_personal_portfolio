1b556656d6a2cd82eb4c30aff5df654bc9fc8eb9
Elliott Chong <elliottchong16@gmail.com>
2022-08-06 16:00:04 +0800

    finalise

--- javascript/career.js
+++ javascript/career.js
@@ -1,4 +1,5 @@
 const nav_elt = document.querySelector("nav");
+const scroll_down_elt = document.querySelector("#scroll-down");
 document.addEventListener("scroll", () => {
   if (window.scrollY >= window.innerHeight) {
     nav_elt.classList.add("navbar-dark");
@@ -8,3 +9,7 @@ document.addEventListener("scroll", () => {
     nav_elt.classList.remove("bg-dark");
   }
 });
+
+scroll_down_elt.onclick = () => {
+  window.scrollTo(0, document.getElementById("career-section").offsetTop - 80);
+};
